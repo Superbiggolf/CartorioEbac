d9639e36a4ef23805c7a6e42cc832cc6b40a5bee
Superbiggolf <70957900+Superbiggolf@users.noreply.github.com>
2025-02-15 17:48:40 -0300

    Opção de sair do programa

--- Cartorio4.c
+++ Cartorio4.c
@@ -21,7 +21,8 @@ int main()
 
 		printf("\t1- Registrar nomes\n");
 		printf("\t2- Consultar nomes\n");
-		printf("\t3- Deletar nomes\n\n");
+		printf("\t3- Deletar nomes\n");
+		printf("\t4- Sair do programa\n\n");
 		printf("Opção: ");
 	
 		scanf("%d", &opcao); //%d armazena um valor inteiro
@@ -40,6 +41,11 @@ int main()
 			
 			case 3:
 			deletar();
+			break;
+			
+			case 4:
+			printf("Obrigado por utilizar o sistema\n");
+			return 0;	
 			break;
 			
 			default:
